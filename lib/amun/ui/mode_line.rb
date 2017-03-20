9717b22deff6e8bc606e12654d6dd471619d6f43
Emad Elsaid <emad.elsaid@blacklane.com>
2017-03-20 16:30:22 +0100

    some refactoring

--- lib/amun/ui/mode_line.rb
+++ lib/amun/ui/mode_line.rb
@@ -4,6 +4,9 @@ require 'amun/ui/mode_line_segments/buffer_name'
 
 module Amun
   module UI
+    # a line of small segments that display
+    # information about the current buffer,
+    # like mode name, line number, buffer name...etc
     class ModeLine
       attr_reader :left_segments, :right_segments
 
@@ -19,19 +22,22 @@ module Amun
       end
 
       def render(window)
-        right_output = right_segments.map do |segment|
-          segment.render(window)
-        end.flatten
-
-        left_output = left_segments.map do |segment|
-          segment.render(window)
-        end.flatten
+        right_output = render_segments(right_segments, window)
+        left_output = render_segments(left_segments, window)
 
         size = (right_output + left_output).map(&:size).inject(0, :+)
         empty_space = [0, window.maxx - size].max
         filler = (' ' * empty_space).colorize(:mode_line)
 
         Helpers::Colors.print(window, *left_output, filler, *right_output)
+      end
+
+      private
+
+      def render_segments(segments, window)
+        segments.map do |segment|
+          segment.render(window)
+        end.flatten
       end
     end
   end
